82ece4db104149eac3ba3ee70390918b29a28143
Torin Donahue <torin.donahue95@gmail.com>
2020-07-27 21:48:06 -0400

    reducers commit

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import entitiesReducer from "./entities_reducer";
+import errorsReducer from "./errors_reducer";
 
 const rootReducer = combineReducers({
   entities: entitiesReducer,
+  errors: errorsReducer,
 });
 export default rootReducer;
